5fe2d0f28fc8e23f3d1fb8394f2f766735d20dda
DintyGadget <windowstar24to7@gmail.com>
2021-05-13 05:22:44 +0900

    test2

--- src/server/game/Handlers/GuildHandler.cpp
+++ src/server/game/Handlers/GuildHandler.cpp
@@ -18,12 +18,11 @@
 #include "WorldSession.h"
 #include "AchievementPackets.h"
 #include "Common.h"
-#include "Creature.h"
+#include "GameTime.h"
 #include "Guild.h"
 #include "GuildMgr.h"
 #include "GuildPackets.h"
 #include "Log.h"
-#include "Map.h"
 #include "ObjectMgr.h"
 #include "Player.h"
 #include "World.h"
@@ -34,7 +33,7 @@ void WorldSession::HandleGuildQueryOpcode(WorldPackets::Guild::QueryGuildInfo& q
         GetPlayerInfo().c_str(), query.GuildGuid.ToString().c_str(), query.PlayerGuid.ToString().c_str());
 
     if (Guild* guild = sGuildMgr->GetGuildByGuid(query.GuildGuid))
-        //if (guild->IsMember(query.PlayerGuid))
+        if (guild->IsMember(query.PlayerGuid))
         {
             guild->SendQueryResponse(this, query.PlayerGuid);
             return;
@@ -388,7 +387,7 @@ void WorldSession::HandleRequestGuildRewardsList(WorldPackets::Guild::RequestGui
         std::vector<GuildReward> const& rewards = sGuildMgr->GetGuildRewards();
 
         WorldPackets::Guild::GuildRewardList rewardList;
-        rewardList.Version = uint32(time(nullptr));
+        rewardList.Version = GameTime::GetGameTimeSystemPoint();
         rewardList.RewardItems.reserve(rewards.size());
 
         for (uint32 i = 0; i < rewards.size(); i++)
